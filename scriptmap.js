3cb149ee6bfce32eed0f050604c73e6b677d682b
Joris Budin <jorisbudin@gmail.com>
2024-03-12 23:30:30 +0100

    Ajout d'un carousel d'images

--- scriptmap.js
+++ scriptmap.js
@@ -1,6 +1,17 @@
-$(".linklist").hover(function (){
+/*$(".linklist").hover(function (){
     $("#"+ $(this).data('txttodisplay')).css("display", "block");
  },
  function (){
     $("#"+ $(this).data('txttodisplay')).css("display", "none");
- });
+ }); */
+
+ $('.carrousel').slick(
+);
+
+ console.log(caca)
+
+ /*   arrows: true,
+   dots: false,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+ }*/
