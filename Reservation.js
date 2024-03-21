04af394fcb4b3a250032f93304e8d8647f26be0f
Joris Budin <jorisbudin@gmail.com>
2024-03-21 23:29:18 +0100

    fix des la moitier du site

--- Reservation.js
+++ Reservation.js
@@ -1,1 +1,22 @@
 
+const reserv = document.getElementsByClassName('card-button');
+const popup = document.getElementById('popup');
+for (let i = 0; i < reserv.length; i++) {
+    reserv[i].addEventListener(('click'), () => {
+        popup.classList.remove("hidden")
+        popup.classList.add("customForm")
+    })
+}
+
+const X = document.getElementById('close');
+
+X.addEventListener(('click'), () => {
+    popup.classList.add("hidden")
+    popup.classList.remove("customForm")
+    
+})
+const resa = document.getElementById("reserveButton");
+resa.addEventListener(('click'), () => {
+    alert("Votre table est bien réservé !")
+    popup.classList.add("hidden")
+    popup.classList.remove("customForm")});
