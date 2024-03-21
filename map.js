607adc251b0b7de73dc0a8979655ebb84751792b
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-03-21 15:02:11 +0100

    responsive nav

--- map.js
+++ map.js
@@ -20,24 +20,59 @@ if (window.matchMedia("(min-width: 600px)").matches) {
     let diva = document.getElementById("diva")
     let ul = document.createElement("ul");
     diva.appendChild(ul);
-    ul.setAttribute("id", "navul");
+    ul.setAttribute("class", "navul");
+    ul.setAttribute("id", "navdesk");
      function list(array) {
         for (i in array) {
             let li = document.createElement("li");
             let lien = document.createElement("a");
-            lien.innerText = index[i]
-            lien.setAttribute('href', liens[i])
-            li.appendChild(lien) 
+            lien.innerText = index[i];
+            lien.setAttribute('href', liens[i]);
+            li.appendChild(lien);
             ul.appendChild(li);
-
-            
 
         }
     
      }
-     let liens = ["accueil.html", "Restaurants.html", "A propos.html"]
 
-     let index = ["Accueil", "Restaurants", "A propos"]
-     list(index)
+    
+    let liens = ["accueil.html", "Restaurants.html", "A propos.html"]
+    
+    let index = ["Accueil", "Restaurants", "A propos"]
+    list(index)
 
+} /*else {
+
+  let ulli = document.getElementById("navdesk");
+  console.log(ulli)
+  ulli.classList.remove("navul");
+  ulli.classList.add("hidden");
+  console.log("toto")
+}*/
+
+let size = 0;
+let newSize = 0;
+if (window.matchMedia("(min-width: 600px)").matches) {
+
+    size = 1;
+
+} else {
+    size = 2;
 }
+
+function compare() {
+    if (window.matchMedia("(min-width: 600px)").matches) {
+
+        newSize = 1;
+    
+    } else {
+        newSize = 2;
+    }
+
+    if (size !== newSize) {
+
+        return location.reload();
+    }
+}
+
+setInterval("compare()", 100);
